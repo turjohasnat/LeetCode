30ec6e19dc760d910dfd05f9b84ea1f81b1ab112
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-02-15 19:13:19 +0600

    Create Solution.cpp

---
+++ C++/88. Merge Sorted Array/Solution.cpp
@@ -0,0 +1,13 @@
+//C++ Solution:
+
+//Runtime: 0 ms
+//Memory Usage: 12.4 MB
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        nums1.resize(m);
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+    }
+};
