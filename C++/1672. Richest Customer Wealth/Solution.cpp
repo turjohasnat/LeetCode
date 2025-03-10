8380119ffc6e6a74a05041f907aaa05759865e25
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-03-10 16:37:21 +0600

    Create Solution.cpp

---
+++ C++/1672. Richest Customer Wealth/Solution.cpp
@@ -0,0 +1,22 @@
+//C++ Solution:
+
+//Runtime: 0 ms
+//Memory Usage: 11.4 MB
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max_wealth = 0;
+
+        for(int i = 0; i < accounts.size(); i++){
+            int total_wealth = 0;
+
+            for(int j = 0; j < accounts[i].size(); j++){
+                total_wealth += accounts[i][j];
+            }
+
+            max_wealth = max(max_wealth, total_wealth);
+        }
+        return max_wealth;
+    }
+};
