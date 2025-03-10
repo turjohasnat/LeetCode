4d977801b01d97e969163d8a51cf622189d2c376
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-03-10 16:15:29 +0600

    Create Solution.cpp

---
+++ C++/448. Find All Numbers Disappeared in an Array/Solution.cpp
@@ -0,0 +1,25 @@
+//C++ Solution:
+
+//Runtime: 3 ms
+//Memory Usage: 53 MB
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); i++){
+            int index = abs(nums[i]) - 1;
+
+            if(nums[index] > 0){
+                nums[index] *= -1;
+            }
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0)
+                result.push_back(i+1);
+        }
+        return result;
+    }
+};
