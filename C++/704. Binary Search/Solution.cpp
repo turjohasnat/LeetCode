10f91cdad03f2f769636c8994eff6892853c19c9
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-03-11 18:49:56 +0600

    Create Solution.cpp

---
+++ C++/704. Binary Search/Solution.cpp
@@ -0,0 +1,24 @@
+//C++ Solution:
+
+//Runtime: 0 ms
+//Memory Usage: 31.4 MB
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int LB = 0, UB = nums.size() - 1;
+        int MID = (LB + UB)/2;
+
+        while(LB <= UB){
+            if(nums[MID] == target)
+                return MID;
+            else if(nums[MID] < target)
+                LB = MID + 1;
+            else
+                UB = MID - 1;
+            
+            MID = (LB + UB)/2;
+        }
+        return -1;
+    }
+};
