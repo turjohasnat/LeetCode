dfb6d2ae551db2fd82c0982189ef1d2abf66e9c3
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-07-03 12:47:36 +0600

    Update Solution.cpp

--- C++/1295. Find Numbers with Even Number of Digits/Solution.cpp
+++ C++/1295. Find Numbers with Even Number of Digits/Solution.cpp
@@ -1,23 +1,21 @@
 //C++ Solution:
 
-//Runtime: 4 ms
-//Memory Usage: 13.4 MB
+//Runtime: 0 ms
+//Memory Usage: 14.5 MB
 
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
-        int even_digits = 0;
-        for(int i = 0; i < nums.size(); i++){
-            int digits = 0;
-            int num = nums[i];
-            while(num != 0){
-                num /= 10;
-                digits++;
-            }
+        int total_digits = 0;
 
-            if(digits % 2 == 0)
-                even_digits++;
+        for(int i = 0; i < nums.size(); i++){
+            string num = to_string( nums[i] );
+
+            int size = num.size();
+            if(size % 2 == 0)
+                total_digits++;
         }
-        return even_digits;
+
+        return total_digits;
     }
 };
