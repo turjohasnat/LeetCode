177896ea0e871823f7d53483dde36dc525442597
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-07-03 20:34:08 +0600

    Update Solution.cpp

--- C++/1207. Unique Number of Occurrences/Solution.cpp
+++ C++/1207. Unique Number of Occurrences/Solution.cpp
@@ -1,22 +1,20 @@
 //C++ Solution:
 
 //Runtime: 0 ms
-//Memory Usage: 12 MB
+//Memory Usage: 12.2 MB
 
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> freq;
-        for(int i = 0; i < arr.size(); i++){
-            freq[arr[i]]++;
-        }
+        unordered_map<int, int> mp;
+        for(auto u : arr)
+            mp[u]++;
 
-        unordered_set<int> unique_num;
-        for(auto u : freq){
-            unique_num.insert(u.second);
-        }
-
-        if(freq.size() == unique_num.size())
+        set<int> st;
+        for(auto u : mp)
+            st.insert(u.second);
+        
+        if(st.size() == mp.size())
             return true;
         else 
             return false;
