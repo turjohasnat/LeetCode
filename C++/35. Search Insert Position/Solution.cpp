a358b7f930d3a7764994b06722572b03adf12bdc
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-07-05 12:16:42 +0600

    Update Solution.cpp

--- C++/35. Search Insert Position/Solution.cpp
+++ C++/35. Search Insert Position/Solution.cpp
@@ -4,14 +4,24 @@
 //Memory Usage: 13.6 MB
 
 class Solution {
+
+    int isOk(vector<int>& nums, int x, int mid){
+        if(nums[mid] < x) return 0;
+        else return 1;
+    }
+
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int cnt = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == target) return i;
-            else if(nums[i] > target) return cnt;
-            cnt++;
+        int left = 0, right = nums.size();
+        int x = target;
+
+        while(left < right){
+            int mid = (left + right) / 2;
+
+            if(isOk(nums, x, mid) == 0) left = mid + 1;
+            else right = mid;
         }
-        return cnt;
+
+        return left;
     }
 };
