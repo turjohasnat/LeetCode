f7edd97983ef2676eadd232126b0ede448c89da3
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-07-18 21:30:08 +0600

    Update Solution.cpp

--- C++/33. Search in Rotated Sorted Array/Solution.cpp
+++ C++/33. Search in Rotated Sorted Array/Solution.cpp
@@ -1,32 +1,44 @@
 //C++ Solution:
 
 //Runtime: 0 ms
-//Memory Usage: 15.1 MB
+//Memory Usage: 15.3 MB
 
 class Solution {
+    int isOk(vector<int>& nums, int left, int mid, int right, int target) {
+        if (nums[mid] == target) 
+            return 2;
+        
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) 
+                return 0;
+            else 
+                return 1;
+        } 
+        else {
+            if (nums[mid] < target && target <= nums[right]) 
+                return 1;
+            else 
+                return 0;
+        }
+    }
+
 public:
     int search(vector<int>& nums, int target) {
-        int lb = 0, ub = nums.size() - 1;
+        int left = 0;
+        int right = nums.size() - 1;
 
-        while(lb <= ub){
-            int mid = (lb + ub)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int result = isOk(nums, left, mid, right, target);
 
-            if(nums[mid] == target){
+            if (result == 2) 
                 return mid;
-            }
-            else if(nums[lb] <= nums[mid]){
-                if(nums[lb] <= target && nums[mid] > target)
-                    ub = mid - 1;
-                else 
-                    lb = mid + 1;
-            }
-            else{
-                if(nums[mid] < target && target <= nums[ub])
-                    lb = mid + 1;
-                else
-                    ub = mid - 1;
-            }
+            else if (result == 0) 
+                right = mid - 1;
+            else 
+                left = mid + 1;
         }
+
         return -1;
     }
 };
