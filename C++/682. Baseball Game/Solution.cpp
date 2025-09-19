fa2c249965b01bedae4334e6885f30a1a86162af
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-09-19 10:59:37 +0600

    Create Solution.cpp

---
+++ C++/682. Baseball Game/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+        for(string i : operations){
+            if(i == "+"){
+                int a = st.top();
+                st.pop();
+                int s = a + st.top();
+                st.push(a);
+                st.push(s);
+            }
+            else if(i == "C"){
+                st.pop();
+            }
+            else if(i == "D"){
+                int a = 2 * st.top();
+                st.push(a);
+            }
+            else{
+                int num = stoi(i);
+                st.push(num);
+            }
+        }
+
+        int sum = 0;
+        while(!st.empty()){
+            sum += st.top();
+            st.pop();
+        }
+
+        return sum;
+    }
+};
